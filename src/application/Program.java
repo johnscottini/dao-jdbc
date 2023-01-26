68c0f143dedbfe4f258a8d565a1d785e72f61b34
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-01-26 19:49:49 -0300

    Implementing findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
-		Seller seller = new Seller(21, "joao", "joao@gmail", new Date(), 3000.0, obj);
 		SellerDao sellerDao= DaoFactory.createSellerDao();
 		
+		Seller seller= sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
