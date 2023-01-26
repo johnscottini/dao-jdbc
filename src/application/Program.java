5bb003ca2eae5cedf687473d6891e9a22e87a49d
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-01-26 20:36:28 -0300

    Implementing find All

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,11 @@ public class Program {
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
+		
+
+		System.out.println("\n====TEST 3:seller find All====");
+		List<Seller> list2 = sellerDao.findAll();
+		list2.forEach(System.out::println);
 	}
 
 }
