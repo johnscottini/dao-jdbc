4e891cba0d9cc3b52b945d7d955ae00554ca3197
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-01-27 14:17:22 -0300

    Implementing and testing Department Dao

--- src/model/Dao/DaoFactory.java
+++ src/model/Dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.Dao;
 
 import db.DB;
+import model.Dao.impl.DepartmentDaoJDBC;
 import model.Dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,4 +9,10 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+
+
+
 }
