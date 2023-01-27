2fe442e20518be5dbf69026bc086dd6ad5e8543a
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-01-27 13:37:36 -0300

    Implementing Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.Dao.DaoFactory;
@@ -25,6 +26,12 @@ public class Program {
 		System.out.println("\n====TEST 3:seller find All====");
 		List<Seller> list2 = sellerDao.findAll();
 		list2.forEach(System.out::println);
+	
+		System.out.println("\n====TEST 4:seller insert====");
+		Seller newSeller= new Seller(null, "greg", "greg@gmail", new Date(), 4000.0, department );
+		sellerDao.insert(newSeller);
+		System.out.println("inserted! New id: "+newSeller.getId());
+		
 	}
 
 }
